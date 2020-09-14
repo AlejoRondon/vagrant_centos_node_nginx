32e125ed31eb22e24b60c9c370a1afe0c42820b2
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-14 18:56:39 -0500

    [ADD] clone_vagrant_repo.sh

---
+++ clone_vagrant_repo.sh
@@ -0,0 +1,3 @@
+sudo mkdir /home/vagrant/documents
+sudo cd /home/vagrant/documents
+sudo git clone https://AlejandroParqueExplora@bitbucket.org/parqueexploradevelopers/vagrant_centos_node_nginx.git
