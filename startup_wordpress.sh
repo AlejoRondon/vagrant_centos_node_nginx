2fa30777a8515f8b981144de828d97a905c2ebae
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-15 11:24:29 -0500

    [EDIT] startup_wordpress.sh, fixing some permissions and little bugs

--- startup_wordpress.sh
+++ startup_wordpress.sh
@@ -8,22 +8,27 @@ sudo wget http://wordpress.org/latest.tar.gz
 echo "<------>Uncompressing file"
 sudo tar xfz latest.tar.gz
 # sudo cp -r wordpress/ /var/www/  
-
-echo "<------>Creating symbolic link to /etc/nginx/sites-available/wordpress"
-sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
 echo "<------>Removing compressed file"
 sudo rm -f latest.tar.gz
+echo "<------>Creating info.php"
+cd /var/www/wordpress
+sudo bash -c 'echo "<?php phpinfo(); ?>" > info.php'
+sudo chown root:root /var/www/wordpress/info.php
+echo "<------>Updating wordpress file permissions"
+sudo chown -R root /var/www/wordpress
+sudo chgrp -R root /var/www/wordpress
+sudo chown nginx:nginx /var/www/wordpress
+sudo chmod -R 775 /var/www/wordpress 
+sudo chcon -t httpd_sys_content_t /var/www/wordpress -R
+sudo chcon -t httpd_sys_rw_content_t /var/www/wordpress -R
 echo "<------>Updating nginx configuration files"
 sudo cp /home/vagrant/documents/vagrant_centos_node_nginx/configuration_files/etc/nginx/nginx.conf /etc/nginx/nginx.conf 
 sudo cp /home/vagrant/documents/vagrant_centos_node_nginx/configuration_files/etc/nginx/sites-available/wordpress /etc/nginx/sites-available/wordpress
-echo "<------>Restarting NGINX"
-sudo systemctl restart nginx
-
-echo "<------>Creating info.php"
-cd /home/vagrant/documents/
-sudo echo "<?php phpinfo(); ?>" >> info.php
-sudo mv ./info.php /var/www/wordpress/
-sudo chown nginx:nginx /var/www/wordpress/info.php
+echo "<------>Creating symbolic link to /etc/nginx/sites-available/wordpress"
+sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
+echo "<------>Restarting NGINX & PHP-FPM services"
+sudo systemctl restart php-fpm.service
+sudo systemctl restart nginx.service
 
 echo "<------>Download and configure the Fork version of WP4PG in your WordPress directory"
 #https://medium.com/@shoaibhassan_/install-wordpress-with-postgresql-using-apache-in-5-min-a26078d496fb
@@ -35,10 +40,10 @@ sudo cp pg4wp/db.php db.php
 
 echo "<------>Creating and editing wp-config.php"
 cd /var/www/wordpress
-sudo cp wp-config-sample.php wp-config2.php
-sudo sed -i 's/database_name_here/dbeduvialwp/g' /var/www/wordpress/wp-config2.php
-sudo sed -i 's/username_here/dbeduvialwpuser1/g' /var/www/wordpress/wp-config2.php
-sudo sed -i 's/password_here/explorauser1/g' /var/www/wordpress/wp-config2.php
+sudo cp wp-config-sample.php wp-config.php
+sudo sed -i 's/database_name_here/dbeduvialwp/g' /var/www/wordpress/wp-config.php
+sudo sed -i 's/username_here/dbeduvialwpuser1/g' /var/www/wordpress/wp-config.php
+sudo sed -i 's/password_here/explorauser1/g' /var/www/wordpress/wp-config.php
 
 echo "<------>Editing wordpress folder permissions"
 sudo chown nginx:nginx /var/www/wordpress
@@ -47,7 +52,7 @@ sudo chcon -t httpd_sys_content_t /var/www/wordpress -R
 sudo chcon -t httpd_sys_rw_content_t /var/www/wordpress -R
 
 
-sudo curl localhost/info.php
+# sudo curl localhost/info.php
 echo "<------>PLEASE VISIT localhost:8080/info.php on the host machine"
 
 
