f686edcedcf387dd478b0a46562ad13483653204
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-15 11:19:19 -0500

    [EDIT] clone_vagrant_repo.sh, command to clone was edited to clone submodules also

--- clone_vagrant_repo.sh
+++ clone_vagrant_repo.sh
@@ -1,3 +1,3 @@
 sudo mkdir /home/vagrant/documents
 sudo cd /home/vagrant/documents
-sudo git clone https://AlejandroParqueExplora@bitbucket.org/parqueexploradevelopers/vagrant_centos_node_nginx.git
+sudo git clone --recurse-submodules -j8 https://AlejandroParqueExplora@bitbucket.org/parqueexploradevelopers/vagrant_centos_node_nginx.git
